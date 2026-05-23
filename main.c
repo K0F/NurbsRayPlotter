53064aa57e388e39d4ad4b833ffcb29af7341d53
Kryštof Pešek <krystof.pesek@gmail.com>
2026-05-23 09:33:54 +0200

    text rendering

--- main.c
+++ main.c
@@ -23,10 +23,18 @@ Wed May 13 02:51:02 AM CEST 2026
 #include "raylib.h"
 #include "geometry_bridge.h"
 #include <math.h>
+#include <stddef.h>
 
 #define NUM_SAMPLES 36
 #define CURVE_RESOLUTION 256.0f
 #define TARGET_FPS 50
+
+#define FONT_SIZE 12
+#define RENDER true
+
+#ifndef PI
+    #define PI 3.14159265358979323846f
+#endif
 
 int frameCount = 0;
 
@@ -38,17 +46,33 @@ int main() {
 
   Vec2 samples[NUM_SAMPLES];
 
+    int baseFontSize = FONT_SIZE;
+    int charCount = 127;
+    // this fixes pixelfont
+    int spacing = 1;
+
+  Font pixelFont = LoadFontEx("resources/Monaco_Linux.ttf", baseFontSize, NULL, 0);
+
+    // 2. Force Point Filtering to ensure perfect pixel boundaries
+  SetTextureFilter(pixelFont.texture, TEXTURE_FILTER_POINT);
+
+
   // FPS
   SetTargetFPS(TARGET_FPS);
+
 
   // main loop
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(BLACK);
 
+    Vector2 pos1 = {40.0,30.0};
+    DrawTextEx(pixelFont, TextFormat("frameCount: %05d", frameCount) , pos1, (float)baseFontSize, spacing, WHITE);
+
+
     for (int i = 0; i < NUM_SAMPLES; i++) {
       float x = i * 1.0f;
-      float y = sinf(x + (frameCount/10.0) + i/10.0) * 50.0f;
+      float y = sinf(x + (frameCount / 250.0f * PI) + (i/NUM_SAMPLES*PI) ) * 50.0f;
       samples[i] = (Vec2){ x * 18.0f + 65.0f, 240.0f - y };
     }
 
@@ -61,17 +85,19 @@ int main() {
     for (int i = 1; i <= CURVE_RESOLUTION; i++) {
       float t = (float)i / CURVE_RESOLUTION;
       Vec2 current = EvaluateCurve(curve, t);
-      DrawLineEx((Vector2) {prev.x, prev.y}, (Vector2){current.x, current.y}, 2.0f, ORANGE);
+      DrawLineEx((Vector2) {prev.x, prev.y}, (Vector2){current.x, current.y}, 1.0f, ORANGE);
       prev = current;
     }
 
     // Draw original points for reference
-    for (int i = 0; i < NUM_SAMPLES; i++) DrawCircle(samples[i].x, samples[i].y, 3, WHITE);
+    for (int i = 0; i < NUM_SAMPLES; i++) DrawCircle(samples[i].x, samples[i].y, 2.0f, WHITE);
+
     EndDrawing();
 
-    const char *fileName = TextFormat("render/frame_%05d.png", frameCount);
-    TakeScreenshot(fileName);
-
+    if(RENDER){
+      const char *fileName = TextFormat("render/frame_%05d.png", frameCount);
+      TakeScreenshot(fileName);
+    }
     DestroyCurve(curve);
     frameCount++;
 
